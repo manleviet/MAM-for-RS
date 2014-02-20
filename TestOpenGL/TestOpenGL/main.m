1b098dc5d880329f54b012e47404ef4e0af5cf3e
duchce <duchce@gmail.com>
2014-02-20 11:12:01 +0700

    commit code add ảnh
    mới add được vài ảnh, chưa có move

---
+++ TestOpenGL/TestOpenGL/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TestOpenGL
+//
+//  Created by MAC PRO on 2/13/14.
+//  Copyright (c) 2014 MAC PRO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
